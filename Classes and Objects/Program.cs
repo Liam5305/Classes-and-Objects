28a6d5eb37d023121e597d63b08c0ea4e6cf2db1
Liam Rutherford <liamrutherford10@gmail.com>
2024-10-02 17:31:21 +0100

    Add project files.

---
+++ Classes and Objects/Program.cs
@@ -0,0 +1,19 @@
+﻿using System; // Classes and Objects
+class Person
+{
+    private string name;
+    public string Name
+    {
+        get { return name; }
+        set { name = value; }
+    }
+}
+class Program
+{
+    static void Main(string[] args)
+    {
+        Person p = new Person();
+        p.Name = "John Doe";
+        Console.WriteLine("Person's Name: {0}", p.Name);
+    }
+}
